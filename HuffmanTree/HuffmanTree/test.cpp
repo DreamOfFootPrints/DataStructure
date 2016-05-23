a05a59fd7d790d1cbac601bc9d7ecafa59958b69
DrreamOfFootPrints <2822850867@qq.com>
2016-05-24 07:22:47 +0800

    哈夫曼树

---
+++ HuffmanTree/HuffmanTree/test.cpp
@@ -0,0 +1,17 @@
+#include"HuffmanTree.h"
+
+
+
+void Test1()
+{
+	int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	HuffmanTree<int> t(arr, len);
+}
+
+int main()
+{
+	Test1();
+	system("pause");
+	return 0;
+}
